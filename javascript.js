8f376cb1585842b482b13d5411765565c644c9cf
Mibo <meebsawosika@gmail.com>
2024-05-21 02:01:02 -0600

    Create function to play a round
    Increase count for winner of the round and announce result of round on
    the console

--- javascript.js
+++ javascript.js
@@ -25,6 +25,33 @@ function getHumanChoice () {
 let humanScore = 0;
 let computerScore = 0;
 
+// Function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
 function playRound (humanChoice,computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`You Tie! Choosing the same option`);
+    } else if (humanChoice === "paper" && computerChoice === "rock" ) {
+        console.log("You win! Paper beats Rock!");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors" ) {
+        console.log("You lose! Scissors beats Paper!");
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper" ) {
+        console.log("You lose! Paper beats Rock!");
+        computerScore++;
+    }  else if (humanChoice === "rock" && computerChoice === "scissors" ) {
+        console.log("You win! Rock beats Scissors!");
+        humanScore++;
+    }  else if (humanChoice === "scissors" && computerChoice === "rock" ) {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore++;
+    }   else if (humanChoice === "scissors" && computerChoice === "paper" ) {
+        console.log("You win! Scissors beats Paper!");
+        humanScore++;
+    }
 
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
