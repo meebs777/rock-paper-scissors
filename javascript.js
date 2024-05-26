ebaf489d3ccb9376a93471a3ea0ab63eda1f0f8e
Mibo <meebsawosika@gmail.com>
2024-05-25 19:53:58 -0600

    Display scores for game ongoing and game winner
    Display updates in game through DOM manipulation. Moved game winner
    logic into a function

--- javascript.js
+++ javascript.js
@@ -30,35 +30,38 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
+    const div = document.querySelector("div");
+
     // Function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
     function playRound (event) {
         let humanChoice = event.target.innerText;
         humanChoice = humanChoice.toLowerCase();
         let computerChoice = getComputerChoice();
-        console.log(computerChoice);
         if (humanChoice === computerChoice) {
-            console.log(`You Tie! Choosing the same option`);
+            div.innerText = "You Tie! Choosing the same option\n Your score is " + humanScore + "\nThe computers score  is " + computerScore;
         } else if (humanChoice === "paper" && computerChoice === "rock" ) {
-            console.log("You win! Paper beats Rock!");
             humanScore++;
+            div.innerText = "You win! Paper beats Rock!\n Your score is " + humanScore + "\nThe computers score  is " + computerScore;
         } else if (humanChoice === "paper" && computerChoice === "scissors" ) {
-            console.log("You lose! Scissors beats Paper!");
             computerScore++;
+            div.innerText = "You lose! Scissors beats Paper!\n Your score is " + humanScore + "\nThe computers score  is " + computerScore;
         } else if (humanChoice === "rock" && computerChoice === "paper" ) {
-            console.log("You lose! Paper beats Rock!");
             computerScore++;
+            div.innerText = "You lose! Paper beats Rock!\n Your score is " + humanScore + "\nThe computers score  is " + computerScore;
         }  else if (humanChoice === "rock" && computerChoice === "scissors" ) {
-            console.log("You win! Rock beats Scissors!");
             humanScore++;
+            div.innerText = "You win! Rock beats Scissors!\n Your score is " + humanScore + "\nThe computers score  is " + computerScore;
         }  else if (humanChoice === "scissors" && computerChoice === "rock" ) {
-            console.log("You lose! Rock beats Scissors!");
             computerScore++;
+            div.innerText = "You lose! Rock beats Scissors!\n Your score is " + humanScore + "\nThe computers score  is " + computerScore;
         }   else if (humanChoice === "scissors" && computerChoice === "paper" ) {
-            console.log("You win! Scissors beats Paper!");
             humanScore++;
+            div.innerText = "You win! Scissors beats Paper!\n Your score is " + humanScore + "\nThe computers score  is " + computerScore;
         }
 
-        
+        if(humanScore === 5 || computerScore === 5) {
+            gameWinner();
+        }
     }
 
     
@@ -71,15 +74,20 @@ function playGame() {
     
     
 
-    
-    
-    if(humanScore > computerScore) {
-        console.log(`You beat the computer ${humanScore}-${computerScore} after 5 rounds!`);
-    } else if (computerScore > humanScore){
-        console.log(`You lost to the computer ${computerScore}-${humanScore} after 5 rounds!`);
-    } else {
-        console.log(`You tie to the computer ${computerScore}-${humanScore} after 5 rounds!`);
+    function gameWinner() {
+        if(humanScore > computerScore) {
+            console.log(`You beat the computer ${humanScore}-${computerScore} after 5 rounds!`);
+            div.innerText = "You beat the computer" + humanScore + "-" + computerScore + " after 5 rounds!";
+        } else if (computerScore > humanScore){
+            console.log(`You lost to the computer ${computerScore}-${humanScore} after 5 rounds!`);
+            div.innerText = "You lost to the computer "  + humanScore + "-" + computerScore + " after 5 rounds!";
+        } else {
+            console.log(`You tie with the computer ${computerScore}-${humanScore} after 5 rounds!`);
+            div.innerText = "You with the computer"  + humanScore + "-" + computerScore + " after 5 rounds!";
+        }
     }
+    
+    
 }
 
 playGame();
